aa1871308cd02f87c170d2c8ffa082d665bb1c3f
GermaVinsmoke <vaibhav1180@gmail.com>
2019-09-26 20:40:31 +0530

    Added Icons and modified manifest

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
+import HttpsRedirect from 'react-https-redirect';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <HttpsRedirect>
+    <App />
+  </HttpsRedirect>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

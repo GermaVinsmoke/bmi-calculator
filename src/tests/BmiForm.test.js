48d2cecb00e1a51e3c68aec4d9b6fc60cb86576f
GermaVinsmoke <vaibhav1180@gmail.com>
2019-10-27 17:39:53 +0530

    input value test

--- src/tests/BmiForm.test.js
+++ src/tests/BmiForm.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import BmiForm from '../BmiForm';
 
 describe('BmiForm Component', () => {
   let wrapper;
-  const props = {
+  const prop = {
     change: jest.fn()
   };
 
   beforeEach(() => {
-    wrapper = shallow(<BmiForm {...props} />);
+    wrapper = shallow(<BmiForm {...prop} />);
   });
 
   it('renders', () => {
@@ -21,6 +21,22 @@ describe('BmiForm Component', () => {
     const useStateSpy = jest.spyOn(React, 'useState');
     useStateSpy.mockImplementation(init => [init, setState]);
     wrapper.find('form').simulate('submit', { preventDefault() {} });
-    expect(props.change).toHaveBeenCalledTimes(0);
+    expect(prop.change).toHaveBeenCalledTimes(0);
+  });
+
+  it('should update the weight', () => {
+    // const setState = jest.fn();
+    // const useStateSpy = jest.spyOn(React, 'useState');
+    // useStateSpy.mockImplementation(init => [init, setState]);
+
+    const weight = wrapper.find('#weight');
+    weight.simulate('change', { target: { name: 'weight', value: '50' } });
+    expect(wrapper.find('#weight').props().value).toEqual('50');
+  });
+
+  it('should update the height', () => {
+    const height = wrapper.find('#height');
+    height.simulate('change', { target: { name: 'height', value: '176' } });
+    expect(wrapper.find('#height').props().value).toEqual('176');
   });
 });

05e8f818ef849ef84980329f60301facfdbc013a
GermaVinsmoke <vaibhav1180@gmail.com>
2019-10-27 18:51:55 +0530

    Removed form handleSubmit

--- src/tests/BmiForm.test.js
+++ src/tests/BmiForm.test.js
@@ -16,13 +16,13 @@ describe('BmiForm Component', () => {
     expect(wrapper).not.toBeNull();
   });
 
-  it('should return on empty weight or height', () => {
-    const setState = jest.fn();
-    const useStateSpy = jest.spyOn(React, 'useState');
-    useStateSpy.mockImplementation(init => [init, setState]);
-    wrapper.find('form').simulate('submit', { preventDefault() {} });
-    expect(prop.change).toHaveBeenCalledTimes(0);
-  });
+  //   it('should return on empty weight or height', () => {
+  //     const setState = jest.fn();
+  //     const useStateSpy = jest.spyOn(React, 'useState');
+  //     useStateSpy.mockImplementation(init => [init, setState]);
+  //     wrapper.find('form').simulate('submit', { preventDefault() {} });
+  //     expect(prop.change).toHaveBeenCalledTimes(0);
+  //   });
 
   it('should update the weight', () => {
     // const setState = jest.fn();
@@ -38,5 +38,17 @@ describe('BmiForm Component', () => {
     const height = wrapper.find('#height');
     height.simulate('change', { target: { name: 'height', value: '176' } });
     expect(wrapper.find('#height').props().value).toEqual('176');
+  });
+
+  it('should call change', () => {
+    // const realUseState = React.useState;
+    // const stubData = [{ weight: '50', height: '176', date: '27/10/2019' }];
+
+    // jest
+    //   .spyOn(React, 'useState')
+    //   .mockImplementationOnce(() => realUseState(stubData));
+
+    wrapper.find('button').simulate('click');
+    expect(prop.change).toHaveBeenCalledTimes(1);
   });
 });

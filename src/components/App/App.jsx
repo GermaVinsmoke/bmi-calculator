a7dec158d8173ffe246a7ea127643937f2951c02
Larissagilliane <larissagilliane@hotmail.com>
2019-10-30 15:30:41 -0300

    replace all double quotes with single quotes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
-import BmiForm from "../BmiForm/BmiForm";
-import Info from "../Info/Info";
-import Bar from "../Bar/Bar";
-import "./App.css";
-import "materialize-css/dist/css/materialize.min.css";
-const uuidv1 = require("uuid/v4");
+import React, { useState, useEffect } from 'react';
+import BmiForm from '../BmiForm/BmiForm';
+import Info from '../Info/Info';
+import Bar from '../Bar/Bar';
+import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+const uuidv1 = require('uuid/v4');
 
 const App = () => {
-  let initialState = () => JSON.parse(localStorage.getItem("data")) || [];
+  let initialState = () => JSON.parse(localStorage.getItem('data')) || [];
   const [state, setState] = useState(initialState);
   const [data, setData] = useState({});
 
@@ -22,17 +22,17 @@ const App = () => {
   };
 
   const handleDelete = id => {
-    localStorage.setItem("lastState", JSON.stringify(state));
+    localStorage.setItem('lastState', JSON.stringify(state));
     let newState = state.filter(i => {
       return i.id !== id;
     });
     setState(newState);
   };
   const handleUndo = () => {
-    setState(JSON.parse(localStorage.getItem("lastState")));
+    setState(JSON.parse(localStorage.getItem('lastState')));
   };
   useEffect(() => {
-    localStorage.setItem("data", JSON.stringify(state));
+    localStorage.setItem('data', JSON.stringify(state));
     const date = state.map(obj => obj.date);
     const bmi = state.map(obj => obj.bmi);
     let newData = { date, bmi };
@@ -40,19 +40,19 @@ const App = () => {
   }, [state]);
 
   return (
-    <div className="container">
-      <div className="row center">
-        <h1 className="white-text"> BMI Tracker </h1>
+    <div className='container'>
+      <div className='row center'>
+        <h1 className='white-text'> BMI Tracker </h1>
       </div>
-      <div className="row">
-        <div className="col m12 s12">
+      <div className='row'>
+        <div className='col m12 s12'>
           <BmiForm change={handleChange} />
           <Bar labelData={data.date} bmiData={data.bmi} />
           <div>
-            <div className="row center">
-              <h4 className="white-text">7 Day Data</h4>
+            <div className='row center'>
+              <h4 className='white-text'>7 Day Data</h4>
             </div>
-            <div className="data-container row">
+            <div className='data-container row'>
               {state.length > 0 ? (
                 <>
                   {state.map(info => (
@@ -68,18 +68,18 @@ const App = () => {
                   ))}
                 </>
               ) : (
-                <div className="center white-text">No log found</div>
+                <div className='center white-text'>No log found</div>
               )}
             </div>
           </div>
-          {localStorage.getItem("lastState") !== null ? (
-            <div className="center">
-              <button className="calculate-btn" onClick={handleUndo}>
+          {localStorage.getItem('lastState') !== null ? (
+            <div className='center'>
+              <button className='calculate-btn' onClick={handleUndo}>
                 Undo
               </button>
             </div>
           ) : (
-            ""
+            ''
           )}
         </div>
       </div>
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { shallow } from "enzyme";
-import App from "./App";
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
 
-describe("App Component", () => {
+describe('App Component', () => {
   let wrapper;
   beforeEach(() => {
     wrapper = shallow(<App />);
   });
 
-  it("renders", () => {
+  it('renders', () => {
     expect(wrapper).not.toBeNull();
   });
 });

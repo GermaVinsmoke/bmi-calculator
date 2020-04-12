748dcca9b9c2991eac6ea6c7cc2a4fccfed4d501
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-04-12 21:03:43 +0530

    uuid DEPRECATED require changed to import

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import 'materialize-css/dist/css/materialize.min.css';
+import './App.css';
 import BmiForm from '../BmiForm/BmiForm';
 import Info from '../Info/Info';
 import Bar from '../Bar/Bar';
-import './App.css';
-import 'materialize-css/dist/css/materialize.min.css';
-const uuidv1 = require('uuid/v4');
 
 const App = () => {
   let initialState = () => JSON.parse(localStorage.getItem('data')) || [];
@@ -14,7 +14,7 @@ const App = () => {
   const handleChange = val => {
     let heightInM = val.height / 100;
     val.bmi = (val.weight / (heightInM * heightInM)).toFixed(2);
-    val.id = uuidv1();
+    val.id = uuidv4();
     let newVal = [...state, val];
     let len = newVal.length;
     if (len > 7) newVal = newVal.slice(1, len);
@@ -68,8 +68,8 @@ const App = () => {
                   ))}
                 </>
               ) : (
-                <div className='center white-text'>No log found</div>
-              )}
+                  <div className='center white-text'>No log found</div>
+                )}
             </div>
           </div>
           {localStorage.getItem('lastState') !== null ? (
@@ -79,8 +79,8 @@ const App = () => {
               </button>
             </div>
           ) : (
-            ''
-          )}
+              ''
+            )}
         </div>
       </div>
     </div>

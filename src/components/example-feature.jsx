877aee888d0c1e45a975afcc48b2e770983500ef
NXDMN <norvintsl@gmail.com>
2021-08-18 03:28:52 +0800

    added example-feature.jsx

---
+++ src/components/example-feature.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import '../App/App.css';
+
+const example = ({ change }) => {
+	
+	return (
+		<>
+			<div>
+                <h1>Example</h1>
+            </div>
+		</>
+	);
+};
+
+export default example;

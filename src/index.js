5250aeeea6e07f57193c01025e23c77994ad8fc7
dsimpson <daniel.simpson@oeconnection.com>
2023-02-18 20:33:53 -0500

    initial commit

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App/App.jsx";
-
-ReactDOM.render(<App />, document.getElementById("root"));
